a9a4d2de4b9467820832916f103603d7bf713377
Jon Nettleton <jon@solid-run.com>
2018-04-26 15:12:08 +0200

    mxc: gpu_viv: Fix poweroff at boot problems
    When issuing a poweroff command at boot it is possible for a race
    to occur with the interrupt handler (mostly with the VG core) that
    results in the poweroff command failing.  Looking at the code there
    is already a specific mode to fix this, gcvPOWER_OFF_ATPOWERON.
    
    Why not use this so our unused cores are fully powered off if they
    aren't used?
    
    Signed-off-by: Jon Nettleton <jon@solid-run.com>

--- drivers/mxc/gpu-viv/hal/os/linux/kernel/gc_hal_kernel_device.c
+++ drivers/mxc/gpu-viv/hal/os/linux/kernel/gc_hal_kernel_device.c
@@ -2104,7 +2104,7 @@ gckGALDEVICE_Start(
 
             /* Switch to SUSPEND power state. */
             gcmkONERROR(gckHARDWARE_SetPowerManagementState(
-                Device->kernels[i]->hardware, gcvPOWER_OFF_BROADCAST
+                Device->kernels[i]->hardware, gcvPOWER_OFF_ATPOWERON
                 ));
         }
     }
@@ -2117,7 +2117,7 @@ gckGALDEVICE_Start(
 #if gcdENABLE_VG
         /* Switch to SUSPEND power state. */
         gcmkONERROR(gckVGHARDWARE_SetPowerManagementState(
-            Device->kernels[gcvCORE_VG]->vg->hardware, gcvPOWER_OFF_BROADCAST
+            Device->kernels[gcvCORE_VG]->vg->hardware, gcvPOWER_OFF_ATPOWERON
             ));
 #endif
     }
@@ -2275,7 +2275,7 @@ gckGALDEVICE_AddCore(
 
     /* Set default power management state. */
     gcmkONERROR(gckHARDWARE_SetPowerManagementState(
-        Device->kernels[core]->hardware, gcvPOWER_OFF_BROADCAST
+        Device->kernels[core]->hardware, gcvPOWER_OFF_ATPOWERON
         ));
 
     gcmkFOOTER_NO();
